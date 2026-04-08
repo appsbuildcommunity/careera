633bcece6d724ccc6b0a45f1e74fccaa7acac195
bagi <hassanachlou@gmail.com>
2026-04-08 18:08:50 +0100

    add analyse model persiste to db

---
+++ backend/app/database/persiste.py
@@ -0,0 +1,20 @@
+from .connection import get_database
+from datetime import datetime
+from app.models import ProfileAnalysisResponse
+
+async def store_analysis_result(user_id: str, analysis: ProfileAnalysisResponse) -> None:
+    """Store the analysis result in MongoDB."""
+    try:
+        db = get_database()
+        coll = db.profiles_analyse_resulte
+        await coll.insert_one({
+               "user_id": user_id,
+               "profile_summary": analysis.profile.profile_summary,
+               "hard_skills": analysis.profile.hard_skills,
+               "soft_skills": analysis.profile.soft_skills,
+               "recommendations": [rec.model_dump() for rec in analysis.recommendations],
+               "created_at": datetime.now(),
+                   })
+    except Exception as e:
+        print(f"Error storing analysis result: {e}")
+        raise e
